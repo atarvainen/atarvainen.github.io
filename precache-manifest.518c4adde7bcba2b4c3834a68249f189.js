228e497c6bd402c612f4eaa0ba3e2ee2088ae3a0
Antti Tarvainen <anttigt@gmail.com>
2019-02-23 00:09:50 +0200

    Create a React app and publish it to GitHub Pages

---
+++ precache-manifest.518c4adde7bcba2b4c3834a68249f189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "905965ad1835d732aa78",
+    "url": "/atarvainen/atarvainen.github.io/static/css/main.29f5672e.chunk.css"
+  },
+  {
+    "revision": "905965ad1835d732aa78",
+    "url": "/atarvainen/atarvainen.github.io/static/js/main.905965ad.chunk.js"
+  },
+  {
+    "revision": "610eb882b3b605bb5cff",
+    "url": "/atarvainen/atarvainen.github.io/static/js/runtime~main.610eb882.js"
+  },
+  {
+    "revision": "57f539728cd6f93d1676",
+    "url": "/atarvainen/atarvainen.github.io/static/js/2.57f53972.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/atarvainen/atarvainen.github.io/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "99d8d7f8a0b97ef10bf740354d5879c8",
+    "url": "/atarvainen/atarvainen.github.io/index.html"
+  }
+];
