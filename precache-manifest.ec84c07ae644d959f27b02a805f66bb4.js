67c3be717c318d264aec1b0e1bd942f71e45a7a0
Antti Tarvainen <anttigt@gmail.com>
2019-02-23 00:22:36 +0200

    Trying something

---
+++ precache-manifest.ec84c07ae644d959f27b02a805f66bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8face29ad64d3829996",
+    "url": "/static/css/main.29f5672e.chunk.css"
+  },
+  {
+    "revision": "b8face29ad64d3829996",
+    "url": "/static/js/main.b8face29.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "57f539728cd6f93d1676",
+    "url": "/static/js/2.57f53972.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "bd4bf5f4f8ae61526e3c660966051fad",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/atarvainen/atarvainen.github.io/precache-manifest.518c4adde7bcba2b4c3834a68249f189.js"
+  "/precache-manifest.ec84c07ae644d959f27b02a805f66bb4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/atarvainen/atarvainen.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
