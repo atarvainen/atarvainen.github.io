dd15604b8aea18637332ab69f7c36b6d8759d12b
Antti Tarvainen <anttigt@gmail.com>
2019-02-24 13:50:55 +0200

    Made some ui improvements

---
+++ precache-manifest.d2d9ff1906caf77686bc46802b4fb6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bcf9f4deddd35bfb8af",
+    "url": "/static/css/main.143ba65e.chunk.css"
+  },
+  {
+    "revision": "6bcf9f4deddd35bfb8af",
+    "url": "/static/js/main.6bcf9f4d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3ed56b21d94431d13229",
+    "url": "/static/js/2.3ed56b21.chunk.js"
+  },
+  {
+    "revision": "95fae31f2f220dcb2c42fd2aae1aad35",
+    "url": "/static/media/navy-blue-background.95fae31f.jpg"
+  },
+  {
+    "revision": "2b589847df8f7a81c1300224c85f12be",
+    "url": "/index.html"
+  }
+];
