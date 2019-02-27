554866f5d023987ce40e971225dfebae49f855b4
Antti Tarvainen <anttigt@gmail.com>
2019-02-27 15:25:55 +0200

    Fixed some links

---
+++ precache-manifest.c16468342fdf225dfded5df527df0269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "972e69a3b1ffa019ff84",
+    "url": "/static/css/main.143ba65e.chunk.css"
+  },
+  {
+    "revision": "972e69a3b1ffa019ff84",
+    "url": "/static/js/main.972e69a3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3ed56b21d94431d13229",
+    "url": "/static/js/2.3ed56b21.chunk.js"
+  },
+  {
+    "revision": "95fae31f2f220dcb2c42fd2aae1aad35",
+    "url": "/static/media/navy-blue-background.95fae31f.jpg"
+  },
+  {
+    "revision": "99b74401997045d3089084a9366135d6",
+    "url": "/index.html"
+  }
+];
