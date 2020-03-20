270946b7d45e76c15107a2809580d986853d1463
anttigt@gmail.com <anttigt@gmail.com>
2020-03-20 16:26:05 +0200

    new portfolio

---
+++ precache-manifest.d012a75de9bbca1a9e3d9de07b739f79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6085b77896198127ef1012c9966f240b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52023abc7f204e107c9c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "806bf04fb5c8d59fbd07",
+    "url": "/static/js/2.8037753c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8037753c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52023abc7f204e107c9c",
+    "url": "/static/js/main.c4015ad6.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
